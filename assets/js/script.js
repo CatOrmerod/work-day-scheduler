964d3135c2c610c6ccb498f4b18d9bb388a30194
Cat Ormerod <catormerod@gmail.com>
2021-03-27 10:19:15 +1100

    Function added for colour coding - not working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
+function hourColorCode() {
+    var currentHour = moment().hour();
+    $(".time-block").each(function (){
+        console.log($(this));
+        var currentTimeBlock = parseInt($(this).attr("id").split(".hour")[1]);
+        if (currentTimeBlock < currentHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (currentTimeBlock === currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+hourColorCode();
+
